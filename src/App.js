ec561ab4255562baa4ceca22344d29628d4f3679
Pullisepop <114167771+Pullisepop@users.noreply.github.com>
2023-11-07 22:59:56 +0100

    added round profile picture component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+// import RoundProfilePicture from './RoundProfilePicture'; // Import your RoundProfilePicture component
+// import RoundProfilePicture from "./components/ProfilePicture/RoundProfilePicture"; // Import your RoundProfilePicture component
+import RoundProfilePicture from "./components/ProfilePicture/ProfilePicture";
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <RoundProfilePicture />{" "}
+      {/* Render the RoundProfilePicture component here */}
     </div>
   );
 }
