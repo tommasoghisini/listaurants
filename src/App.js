831d8ed7208a67dd8abcb254627b2945f92f43db
pantageepapa <pantageepapa@gmail.com>
2023-11-16 15:03:10 +0100

    Added the notification page to the icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 
-
 import { Routes, Route, useLocation } from "react-router-dom";
-
 
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 import MainPage from "./pages/MainPage/MainPage";
-
 import BottomNavBar from "./components/NavBar/NavBar";
 import { Fragment } from "react";
+import NotificationPage from "./pages/NotificationPage/NotificationPage";
 
 function App() {
 	const location = useLocation();
@@ -25,6 +23,10 @@ function App() {
 				<Route
 					path="/"
 					element={<LoginPage />}
+				/>
+				<Route
+					path="/notification"
+					element={<NotificationPage />}
 				/>
 				<Route
 					path="/profile"
