1ecb20def63886386dee88112c6704a965d12a61
tommasoghisini <tommaso.ghisini@gmail.com>
2023-11-17 21:28:34 +0100

    set up connection to backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import "./App.css";
-
+import React, { Fragment } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
+import Parse from 'parse';
 
+import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
-
 import MainPage from "./pages/MainPage/MainPage";
-import BottomNavBar from "./components/NavBar/NavBar";
-import { Fragment } from "react";
 import NotificationPage from "./pages/NotificationPage/NotificationPage";
 import FriendsPage from "./pages/FriendsPage/FriendsPage";
+import BottomNavBar from "./components/NavBar/NavBar";
+
+const PARSE_APPLICATION_ID = 'LQnwm0hvEVNo9UTc5XH0xryIdfqgKCDelr6ETCSJ';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'ci5dv6jVNkOpT6A2O57lA9fTZK0c8HMWCEtV2eLM';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
 	const location = useLocation();
@@ -17,22 +22,10 @@ function App() {
 	return (
 		<Fragment>
 			<Routes>
-				<Route
-					path="/home"
-					element={<MainPage />}
-				/>
-				<Route
-					path="/"
-					element={<LoginPage />}
-				/>
-				<Route
-					path="/notification"
-					element={<NotificationPage />}
-				/>
-				<Route
-					path="/profile"
-					element={<ProfilePage />}
-				/>
+				<Route path="/home" element={<MainPage />} />
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/notification" element={<NotificationPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
 			</Routes>
 			{location.pathname !== "/" && <BottomNavBar />}
 		</Fragment>
