25570ca830a0d81634575a0e69c112ff39935526
Pullisepop <114167771+Pullisepop@users.noreply.github.com>
2023-11-21 22:42:22 +0100

    clean up code. renamed profilepicture

---
+++ src/components/ProfilePicture2/ProfilePicture2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AliceImage from "../../assets/images/Alice.jpeg";
+import "./ProfilePicture2.css";
+
+function ProfilePicture2() {
+  return (
+    <div className="profile-container">
+      <img className="profile-picture" src={AliceImage} alt="Alice" />
+    </div>
+  );
+}
+
+export default ProfilePicture2;
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ListCards from "../../components/ListCard/listcards";
-import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
+import ProfilePicture2 from "../../components/ProfilePicture2/ProfilePicture2";
 import ButtonSh from "../../components/shared/ButtonShort/ButtonShort";
 import TopBar from "../../components/shared/TopBar/TopBar";
 import "./ProfilePage.css";
@@ -22,7 +22,7 @@ function ProfilePage() {
       <TopBar pageName="Profile" />
       <div className="profile-section">
         <div className="profile-picture">
-          <ProfilePicture />
+          <ProfilePicture2 />
         </div>
         <p className="profile-name">Alice</p>
       </div>
