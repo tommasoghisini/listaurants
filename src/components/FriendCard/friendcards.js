d6d052dd877ec54ccb6037f8fb59deb959ad2337
[lydiastenflo] <lyst22ab@student.cbs.dk>
2023-12-04 11:22:28 +0100

    lydia css changes :)

--- src/components/FriendCard/friendcards.js
+++ src/components/FriendCard/friendcards.js
@@ -1,55 +1,39 @@
 import React from "react";
 import "./friendcards.css";
+import ProfilePicture from "../shared/ProfilePicture/ProfilePicture";
 
 function FriendCard() {
-	const titles = ["Bob Dylan", "Cate Blanchett"];
-	const texts = ["1 friend", "2 friends"];
+  const titles = ["Bob Dylan", "Cate Blanchett"];
+  const texts = ["1 friend", "2 friends"];
 
-	return (
-		<div>
-			{titles.map((title, index) => (
-				<Card
-					key={index}
-					title={title}
-					text={texts[index]}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      {titles.map((title, index) => (
+        <Card key={index} title={title} text={texts[index]} />
+      ))}
+    </div>
+  );
 }
 
 function Card({ title, text }) {
-	const handleRemove = () => {
-		// Implement the logic for removing the friend
-		console.log(`Removing friend: ${title}`);
-	};
+  const handleRemove = () => {
+    console.log(`Removing friend: ${title}`);
+  };
 
-	return (
-		<div className="friend-card">
-			<div className="friend-content">
-				<div className="image-container">
-					<div className="profile-image-border">
-						<div className="profile-image-wrapper">
-							<img
-								src={"/images/bob.jpeg"}
-								alt="Profile"
-								className="profile-image"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="text-container">
-					<h1 className="friend-name">{title}</h1>
-					<h1 className="friend-text">{text}</h1>
-				</div>
-				<button
-					className="remove-button"
-					onClick={handleRemove}>
-					Remove
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="friend-card">
+      <div className="friend-content">
+        <ProfilePicture imgSrc={"/images/bob.jpeg"} height="50px" />
+        <div className="text-container">
+          <h1 className="friend-name">{title}</h1>
+          <h1 className="friend-text">{text}</h1>
+        </div>
+        <button className="remove-button" onClick={handleRemove}>
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default FriendCard;
