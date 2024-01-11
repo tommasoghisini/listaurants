c1b20b00225e115d0fa22df2caee7e612a3584fe
tommasoghisini <tommaso.ghisini@gmail.com>
2024-01-11 11:22:23 +0100

    comments for app and protected route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// import with curly brackets is used with named exports (when you export multiple things from a file)
 import React, { Fragment, useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Parse from "parse/dist/parse.min.js";
+
+// auth context is used to pass the authentication state to all the components
+// that need it without having to pass it through props
 import AuthContext from "./components/AuthContext/AuthContext";
 
 import "./App.css";
+
+// import all the pages
 import LoginPage from "./pages/LoginProcess/LoginPage/LoginPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import FriendsPage from "./pages/FriendsPage/FriendsPage";
@@ -24,15 +30,20 @@ import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// Parse initialization
 const PARSE_APPLICATION_ID = "LQnwm0hvEVNo9UTc5XH0xryIdfqgKCDelr6ETCSJ";
 const PARSE_HOST_URL = "https://parseapi.back4app.com/";
 const PARSE_JAVASCRIPT_KEY = "ci5dv6jVNkOpT6A2O57lA9fTZK0c8HMWCEtV2eLM";
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
+
+// Not sure
 Parse.setAsyncStorage(AsyncStorage);
 
 function App() {
 	const location = useLocation();
+
+	// array of paths that don't need the navbar
 	const noNavBarPaths = [
 		"",
 		"add-picture",
@@ -41,51 +52,33 @@ function App() {
 		"verification",
 		"signup-name",
 	];
+	// state to control navbar visibility
 	const [isNavbarVisible, setIsNavbarVisible] = useState(true);
-	// Listen for changes in the route
+
+	// Listen for changes in the route and reset navbar visibility
 	useEffect(() => {
 		// Reset navbar visibility when the route changes
 		setIsNavbarVisible(true);
 	}, [location.pathname]);
 
+	// Get the base path (right side of /)
 	const basePath = location.pathname.split("/")[1];
 
+	// state to control authentication
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+	// array of routes
 	const routes = [
-		{
-			path: "/",
-			component: LoginPage,
-			isProtected: false,
-			extraProps: { setIsAuthenticated },
-		},
-		{
-			path: "/signup",
-			component: SignupPage,
-			isProtected: false,
-			extraProps: { setIsAuthenticated },
-		},
-		{
-			path: "/home",
-			component: MainPage,
-			isProtected: true,
-			extraProps: { setIsNavbarVisible },
-		},
+		{ path: "/", component: LoginPage, isProtected: false, extraProps: { setIsAuthenticated } },
+		{ path: "/signup", component: SignupPage, isProtected: false, extraProps: { setIsAuthenticated } },
+		{ path: "/home", component: MainPage, isProtected: true, extraProps: { setIsNavbarVisible } },
 		{ path: "/notification", component: NotificationPage, isProtected: true },
 		{ path: "/profile", component: ProfilePage, isProtected: true },
 		{ path: "/friendspage", component: FriendsPage, isProtected: true },
 		{ path: "/list/:listname", component: ListPage, isProtected: true },
 		{ path: "/editprofilepage", component: EditProfilePage, isProtected: true },
-		{
-			path: "/add-picture/:username",
-			component: AddProfilePicture,
-			isProtected: true,
-		},
-		{
-			path: "/added-picture/:username",
-			component: AddedProfilePicture,
-			isProtected: true,
-		},
+		{ path: "/add-picture/:username", component: AddProfilePicture, isProtected: true },
+		{ path: "/added-picture/:username", component: AddedProfilePicture, isProtected: true },
 		{ path: "/verification", component: VerificationPage, isProtected: true },
 		{ path: "/signup-name", component: SignupNamePage, isProtected: true },
 		{ path: "/add", component: AddRestaurantPage1, isProtected: true },
@@ -93,11 +86,15 @@ function App() {
 	];
 
 	return (
+		// AuthContext.Provider is used to pass the authentication state to all the components
 		<AuthContext.Provider value={isAuthenticated}>
 			<Fragment>
 				<div className="app-container">
 					<div className="content">
+						// Routes is wrapper that containes all the routes components
 						<Routes>
+							// Iterate through the routes array and element prop is used to
+							// conditionally render a protected route or a normal route
 							{routes.map((route) => (
 								<Route
 									path={route.path}
@@ -114,6 +111,7 @@ function App() {
 							))}
 						</Routes>
 					</div>
+					// BottomNavBar is rendered only if page is not in the noNavBarPaths array
 					{isNavbarVisible && !noNavBarPaths.includes(basePath) && (
 						<BottomNavBar />
 					)}
--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,20 +2,29 @@ import React, { useContext, useEffect } from 'react';
 import AuthContext from '../AuthContext/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
+// children is the component that will be rendered if the user is authenticated
 const ProtectedRoute = ({ children }) => {
+  // Get the authentication state from the context
   const isAuthenticated = useContext(AuthContext);
+  // Get the navigate function from the router
   const navigate = useNavigate();
 
+  // If the user is not authenticated, redirect to the home page
   useEffect(() => {
     if (!isAuthenticated) {
       navigate('/');
     }
+    // dependency array allows to re-run the effect when the
+    // isAuthenticated state changes or the navigate function changes
   }, [isAuthenticated, navigate]);
 
+
+  // If the user is not authenticated, don't render anything
   if (!isAuthenticated) {
     return null;
   }
 
+  // If the user is authenticated, render the children prop
   return children;
 };
 
